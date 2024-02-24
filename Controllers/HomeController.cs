e5c91d95888868f5672aa95a3b159dd60ad4236f
kananbayramli <bayramli-kenan@mail.ru>
2024-02-25 00:32:14 +0400

    First commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieApp.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieApp.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            string FilmTitle = "film Bashligi";
+            string FilmDescription = "Film Achiqlamasi";
+            string FilmDirector = "Fred Quimby";
+            string[] acters = {"Tom", "Jerry", "Duffy Duck" };
+
+            var m = new Movie();
+            m.Title = FilmTitle;
+            m.Description = FilmDescription;
+            m.Director = FilmDirector;
+            m.Acters = acters;
+
+            return View(m);
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieApp.Web.Models
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Director { get; set; }
+        public string[] Acters { get; set; }
+    }
+}
