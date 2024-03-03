f9c4eb66eb298066b026c9545e8a66501a78ce6e
kananbayramli <bayramli-kenan@mail.ru>
2024-03-03 23:04:12 +0400

    Created Admin/MovieList page

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MovieApp.Web.Data;
+using MovieApp.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieApp.Web.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly MovieContext _context;
+
+        public AdminController(MovieContext context) 
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult MovieList() 
+        {
+            return View(new AdminMoviesViewModel 
+            {
+                Movies = _context.Movies
+                .Include(m => m.Genres)
+                .Select(m => new AdminMovieViewModel
+                {
+                    MovieId = m.MovieId,
+                    ImageUrl = m.ImageUrl,
+                    Title = m.Title,
+                    Genres = m.Genres.ToList()
+                })
+                .ToList()
+            });
+        }
+    }
+}
---
+++ Models/AdminMoviesViewModel.cs
@@ -0,0 +1,21 @@
+﻿using MovieApp.Web.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieApp.Web.Models
+{
+    public class AdminMoviesViewModel
+    {
+        public List<AdminMovieViewModel> Movies { get; set; }
+    }
+
+    public class AdminMovieViewModel 
+    {
+        public int MovieId { get; set; }
+        public string Title { get; set; }
+        public string ImageUrl { get; set; }
+        public List<Genre> Genres { get; set; }
+    }
+}
