3c2eedf852c636a84932e11b04b6f066befc4c5e
kananbayramli <bayramli-kenan@mail.ru>
2024-03-05 19:51:48 +0400

    GenreUpdate/POST edit/delete

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -100,5 +100,55 @@ namespace MovieApp.Web.Controllers
                 }).ToList()
             });
         }
+
+        public IActionResult GenreUpdate(int? id) 
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var entity = _context.Genres
+                .Select(g => new AdminGenreEditViewModel 
+                {
+                    GenreId = g.GenreId,
+                    Name = g.Name,
+                    Movies = g.Movies.Select(i => new AdminMovieViewModel 
+                    { 
+                        MovieId = i.MovieId,
+                        Title = i.Title,
+                        ImageUrl = i.ImageUrl
+                    }).ToList()
+                })
+                .FirstOrDefault(g => g.GenreId == id);
+
+            if (entity == null)
+            {
+                return NotFound();
+            }
+
+            return View(entity);
+        }
+
+        [HttpPost]
+        public IActionResult GenreUpdate(AdminGenreEditViewModel model, int[] movieIds) 
+        {
+            var entity = _context.Genres.Include("Movies").FirstOrDefault(i => i.GenreId == model.GenreId);
+
+            if (entity == null) 
+            {
+                return NotFound();
+            }
+
+            entity.Name = model.Name;
+            foreach (var id in movieIds)
+            {
+                entity.Movies.Remove(entity.Movies.FirstOrDefault(i => i.MovieId == id));
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToAction("GenreList");
+        }
     }
 }
--- Models/AdminGenresViewModel.cs
+++ Models/AdminGenresViewModel.cs
@@ -17,4 +17,11 @@ namespace MovieApp.Web.Models
         public string Name { get; set; }
         public int Count { get; set; }
     }
+
+    public class AdminGenreEditViewModel 
+    {
+        public int GenreId { get; set; }
+        public string Name { get; set; }
+        public List<AdminMovieViewModel> Movies { get; set; }
+    }
 }
