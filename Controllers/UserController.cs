196c84bb3fb7592a033a20d6da34d9a3d0845759
kananbayramli <bayramli-kenan@mail.ru>
2024-03-07 15:24:47 +0400

    User Register page

---
+++ Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieApp.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieApp.Web.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult CreateUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateUser(UserModel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/UserModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieApp.Web.Models
+{
+    public class UserModel
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string RePassword { get; set; }
+        public string Url { get; set; }
+        public string BirthYear { get; set; }
+    }
+}
