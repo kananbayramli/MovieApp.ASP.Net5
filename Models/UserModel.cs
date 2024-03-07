5025f6d827a078bc0114a2c8d6440b50c1147b48
kananbayramli <bayramli-kenan@mail.ru>
2024-03-07 15:39:46 +0400

    Validation Atributes

--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,12 +9,27 @@ namespace MovieApp.Web.Models
     public class UserModel
     {
         public int UserId { get; set; }
+        [Required]
         public string UserName { get; set; }
+        [Required]
         public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
-        public string RePassword { get; set; }
+
+        [Required]
+        [Compare(nameof(Password))]
+        public string RePassword { get; set; }  
+
+        [Url]
         public string Url { get; set; }
-        public string BirthYear { get; set; }
+        
+        [Range(1990, 2006)]
+        public int BirthYear { get; set; }
     }
 }
